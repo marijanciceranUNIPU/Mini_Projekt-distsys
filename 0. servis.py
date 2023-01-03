a07bee3e7e0fdf23a7ad8d83787ee558d71da147
marijanciceran25 <115954072+marijanciceranUNIPU@users.noreply.github.com>
2023-01-03 14:22:07 +0100

    Dodavanje podataka u bazu iz dataseta

--- 0. servis.py
+++ 0. servis.py
@@ -1,5 +1,6 @@
 import aiohttp
 import aiosqlite
+import pandas as pd
 from aiohttp import web
 
 routes = web.RouteTableDef()
@@ -11,10 +12,27 @@ async def json_data(request):
             async with db.execute("SELECT COUNT(*) FROM mini_projekt_db") as cursor:
                 async for row in cursor:
                     if row[0] == 0:
-                        data = print("Baza je prazna.")
+                        print("Baza je prazna.")
+                        try:
+                            dataframe = pd.read_json("file-000000000040.json", lines = True)
+                            async with aiosqlite.connect("dbase.db") as db:
+                                for row in dataframe.tail(10000).iterrows():
+                                    await db.execute(
+                                        "INSERT INTO mini_projekt_db (username, ghublink, filename) VALUES (?, ?, ?)",
+                                        (
+                                            row.get["repo_name"].rsplit("/", 1)[0],
+                                            "https://github.com/" + row.get("repo_name") + ".com",
+                                            row.get("path").rsplit("/", 1)[1]
+                                        )
+                                    )
+                                    await db.commit()
+                            print("Podaci uspješno dodani u bazu.")
+                        except Exception as e:
+                            print("Dogodila se greška: ", e)
+                        pass
                     else:
-                        data = print("Baza nije prazna.")
-        return web.json_response({"name": "service0", "status": "OK", "data": data}, status = 200)
+                        print("Baza nije prazna.")
+        return web.json_response({"name": "service0", "status": "OK"}, status = 200)
     except Exception as e:
         return web.json_response({"name": "service0", "error": str(e)}, status = 500)
 
